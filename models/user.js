34ea9e3ba4a4a86dcc8328cd15c00df7d7d63b8a
ronchitech <ronchibusiness@gmail.com>
2021-06-24 00:41:34 +0800

    clearing the cart after storing the orders

--- models/user.js
+++ models/user.js
@@ -69,6 +69,10 @@ userSchema.methods.removeFromCart = function (prodId) {
   this.cart.items = updatedCart;
   return this.save();
 };
+userSchema.methods.clearCart = function() {
+  this.cart = {items: []}
+  return this.save()
+}
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');
